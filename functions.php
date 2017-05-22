b27695710b589741f1c8f8aee5b5eaed35c95333
jusla3 <jusla3@yahoo.co.uk>
2017-05-22 16:13:48 +0100

    initial upload

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function awesome_script_enqueue() {
+
+	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
+	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', true);
+
+}
+add_action( 'wp_enqueue_scripts', 'awesome_script_enqueue');
+function awsome_theme_setup(){
+	add_theme_support('menus');
+	register_nav_menu('primary','primary Header Navigation');
+}
+add_action('init', 'awsome_theme_setup');
+
---
+++ header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>WORDPRESS 101</title>
+		<?php wp_head(); ?>
+	</head>
+
+	<body>
+		<?php wp_nav_menu(); ?>
